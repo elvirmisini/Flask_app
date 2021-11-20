ca4ceefed46bc1b43a0509d9163f869c7a2d6486
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2021-11-20 17:45:59 +0100

    Title of pdf file, Title of webpage and level languages updated

--- pages.py
+++ pages.py
@@ -16,24 +16,6 @@ class Page(object):
 
 
     def second(self):
-        # if request.method == 'POST':
-        #     Photo = request.form.get('Photo')
-        #     fname = request.form.get('fname')
-        #     lname = request.form.get('lname')
-        #     birthday = request.form.get('birthday')
-        #     email = request.form.get('email')
-        #     address = request.form.get('address')
-        #     DriverLicence = request.form.get('DriverLicence')
-        #     AboutMe = request.form.get('AboutMe')
-        #     Education = request.form.get('Education')
-        #     Experience = request.form.get('Experience')
-        #     Skills = request.form.get('Skills')
-        #     Hobbies = request.form.get('Hobbies')
-        #     Language1 = request.form.get('Language1')
-        #     Language2 = request.form.get('Language2')
-        #     Language3 = request.form.get('Language3')
-        #     Language4 = request.form.get('Language4')
-        #     return redirect(url_for('pdfCV'))
         return render_template('second.html')
 
     def pdf_generate(self):
@@ -53,10 +35,14 @@ class Page(object):
         Language2 = request.form.get('Language2')
         Language3 = request.form.get('Language3')
         Language4 = request.form.get('Language4')
-        return render_template("pdf_template.html",Photo=Photo,fname=fname,lname=lname,birthday=birthday,email=email,address=address,DriverLicence=DriverLicence,AboutMe=AboutMe,Education=Education,Experience=Experience,Skills=Skills,Hobbies=Hobbies,Language1=Language1,Language2=Language2,Language3=Language3,Language4=Language4)
-        # render = render_template("pdf_template.html")
-        # pdf = pdfkit.from_string(render, False)
-        # response = make_response(pdf)
-        # response.headers['Content-Type'] = 'application/pdf'
-        # response.headers['Content-Disposition'] = 'inline;filename=output.pdf'
-        # return response
+        levelL1=request.form.get('levelL1')
+        levelL2=request.form.get('levelL2')
+        levelL3=request.form.get('levelL3')
+        levelL4=request.form.get('levelL4')
+        # return render_template("pdf_template.html",Photo=Photo,fname=fname,lname=lname,birthday=birthday,email=email,address=address,DriverLicence=DriverLicence,AboutMe=AboutMe,Education=Education,Experience=Experience,Skills=Skills,Hobbies=Hobbies,Language1=Language1,Language2=Language2,Language3=Language3,Language4=Language4,levelL1=levelL1,levelL2=levelL2,levelL3=levelL3,levelL4=levelL4)
+        render = render_template("pdf_template.html",Photo=Photo,fname=fname,lname=lname,birthday=birthday,email=email,address=address,DriverLicence=DriverLicence,AboutMe=AboutMe,Education=Education,Experience=Experience,Skills=Skills,Hobbies=Hobbies,Language1=Language1,Language2=Language2,Language3=Language3,Language4=Language4,levelL1=levelL1,levelL2=levelL2,levelL3=levelL3,levelL4=levelL4)
+        pdf = pdfkit.from_string(render, False)
+        response = make_response(pdf)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'inline;filename=CV of '+fname+lname+'.pdf'
+        return response
